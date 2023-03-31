71b239e77bfb1b890c9c0b0db9ee030a44aaa2fb
Marco Ugarte <marcougarte1998@gmail.com>
2023-03-31 17:27:09 -0300

    reset

--- Votacion2.0/js/validadorCheckBox.js
+++
@@ -1,9 +0,0 @@
-//Evento validador de checkbox
-const form = document.querySelector('#my-form');
-form.addEventListener('submit', (event) => {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-    if (checkboxes.length < 2) {
-      event.preventDefault(); // Prevenir que el formulario se envíe
-      alert('Seleccione al menos dos opciones de como se enteró de nosotros.'); // Mostrar mensaje de error
-    }
-});
